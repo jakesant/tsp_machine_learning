42912436b51c439cd108b46b2a22a46f9663c039
Jake Sant <jake.sant.18@um.edu.mt>
2020-12-22 20:36:49 +0100

    Using TSPLIB 95 library to read TSP files

--- travellingsalesman.py
+++ travellingsalesman.py
@@ -1,1 +1,17 @@
-read_file = open('problem.tsp', 'r')
+#The tours in the TSP can be represented as a vector of cities
+#Documentation -> https://tsplib95.readthedocs.io/en/stable/
+
+import tsplib95
+import networkx
+
+tsp = tsplib95.load('burma14.tsp')
+
+#print(tsp.render()) #Outputs the contents of the TSP file
+
+#print(list(tsp.get_nodes()))
+
+print(list(tsp.get_edges()))
+print(tsp.edge_weight_type)
+test_edge = 3, 8
+weight = tsp.get_weight(*test_edge)
+print(weight)
