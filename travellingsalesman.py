d454074cbd22860dcca36f15bc327e47e77610d0
Jake Sant <jake.sant.18@um.edu.mt>
2021-01-17 14:04:42 +0100

    Removed unused code. Implemented SA func using satsp, however not working

--- travellingsalesman.py
+++ travellingsalesman.py
@@ -4,37 +4,27 @@
 import tsplib95
 import networkx
 import acopy
+from satsp import solver
 
-def sa():
+def sa(graph):
     """Simulated Annealing Algorithm
     """
 
-def aco():
+    solver.Solve(city_list = graph, epochs = 400, stopping_count = 500)
+    solver.PrintSolution
+
+def aco(graph):
     """Ant Colony Optimisation Algorithm"""
 
     solver = acopy.Solver(rho=.03, q=1)
     colony = acopy.Colony(alpha=1, beta=3)
-    tour = solver.solve(G, colony, limit=100)
+    tour = solver.solve(graph, colony, limit=500)
     print(tour)
 
-tsp = tsplib95.load('burma14.tsp')
-
-G = tsp.get_graph()
-
-aco()
-print("Ok")
-
-#print(tsp.render()) #Outputs the contents of the TSP file
-
-print(list(tsp.get_nodes())) #Prints all nodes
-print(list(tsp.get_edges())) #Prints all edges
-alledges = tsp.get_edges()
-test_edge = 3, 8
-weight = tsp.get_weight(*test_edge)
-print(weight)
-qw = tsp.get_graph()
-qw.nodes
-
-"""
-print(tsp.edge_weight_type)
-"""
+if __name__=='__main__':
+    tsp = tsplib95.load('burma14.tsp')
+    problem_graph = tsp.get_graph()
+    print("Ant Colony Optimisation test")
+    aco(problem_graph)
+    print("Simulated Annealing test")
+    sa(problem_graph)
